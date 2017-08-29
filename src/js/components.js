c0520bffbcdd4ba2da299be5d06a8a5b0af9fa64
Vu Nguyen <vunguyen@Vus-MacBook-Air.local>
2017-08-29 12:58:42 +0300

    working with IE9 IIE11

--- src/js/components.js
+++ src/js/components.js
@@ -1,82 +1,3 @@
-var searchResult =   {
-    resultItems: [
-      {
-        id: 0,
-        roomName: 'Daenerys Targaryen'
-      },
-      {
-        id: 1,
-        roomName: 'Sokos Classroom A'
-      },
-      {
-        id: 2,
-        roomName: 'Sokos Classroom B'
-      },
-      {
-        id: 3,
-        roomName: 'Sokos Classroom C'
-      }
-    ]
-  }
-
-var meetingLengthOptions = [{
-          value: '2',
-          label: '2h'
-        }, {
-          value: '3',
-          label: '3h'
-        }, {
-          value: '4',
-          label: '4h'
-        }, {
-          value: '5',
-          label: '5h'
-        }, {
-          value: '6',
-          label: '6h'
-        },
-        {
-          value: '7',
-          label: '7h'
-        },
-        {
-          value: '8',
-          label: '8h'
-        },
-        {
-          value: '9',
-          label: '9h'
-        },
-        {
-          value: '10',
-          label: '10h'
-        },
-        {
-          value: '24',
-          label: 'Overnight'
-        },
-        {
-          value: '48',
-          label: '2 Days'
-        },
-      ]
-
-var seatingOrderOptions = [{
-          value: 'U-shape',
-          label: 'U-shape'
-        }, {
-          value: 'BoardRoom',
-          label: 'BoardRoom'
-        }, {
-          value: 'ClassRoom',
-          label: 'ClassRoom'
-        }, {
-          value: 'Banquet',
-          label: 'Banquet'
-        }, {
-          value: 'Theatre',
-          label: 'Theatre'
-        }]
 
 Vue.component('booking', {
 
@@ -99,7 +20,7 @@ Vue.component('search-input-box', {
     }
   },
   methods: {
-      handleChange(value) {
+      handleChange: function(value) {
         // console.log(value)
       }
   }
